9cf4b4be2b3973c5e0fcbaa9eaf531ed5d887740
Mathieu <mathieu@epitech.eu>
2025-08-05 10:11:56 +0200

    n8

--- eslintrc.js
+++ eslintrc.js
@@ -3,26 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: "eslint:recommended",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
+    ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
   rules: {
-    // Ajoute tes règles personnalisées ici
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
+    // Tu peux ajouter ou ajuster des règles ici
+    "no-unused-vars": "warn",
+    "no-console": "off",
   },
 };
